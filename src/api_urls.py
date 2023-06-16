830f0c4251c5109201c6ed4f031306691b7406c6
jllpons <joanlluispons@gmail.com>
2023-06-16 13:25:30 +0200

    initial commit

---
+++ src/api_urls.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+
+UNIPROT_API_URL = 'something/{code}/'
---
+++ src/sd2ba.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""
+sd2ba: Segmented Domain to Breakpoint Analysis
+"""
+
+#from fetch_data import make_uniprot_api_call
+
